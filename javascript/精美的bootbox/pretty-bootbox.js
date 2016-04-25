0c14c3de2a0910eb3006bd8cd442381b7fe3f485
Jason Tseng <tsaistorm@gmail.com>
2016-04-25 15:20:58 +0800

    show waiting dialog

--- javascript/精美的bootbox/pretty-bootbox.js
+++ javascript/精美的bootbox/pretty-bootbox.js
@@ -1,14 +1,14 @@
 /**
  * 以 BootBox 方式顯示成功訊息
  */
-showSuccessBootBox = function(callBack) {
+var showSuccessBootBox = function(callBack) {
     showSuccessBootBoxCustom("操作成功", callBack);
 };
 
 /**
  * 以 BootBox 方式顯示成功訊息, 並傳入自訂義訊息
  */
-showSuccessBootBoxCustom = function(successMsg, callBack) {
+var showSuccessBootBoxCustom = function(successMsg, callBack) {
     var $successDiv = $('div[id="successBootBox"]');
     $('span[id="msgSpan"]', $successDiv).empty().html(successMsg);
 
@@ -23,7 +23,7 @@ showSuccessBootBoxCustom = function(successMsg, callBack) {
 /**
  * 以 BootBox 方式顯示成功警示訊息
  */
-showSuccessWithWarringBootBox = function(successMsg, warringMsg, callBack) {
+var showSuccessWithWarringBootBox = function(successMsg, warringMsg, callBack) {
     var $containerDiv = $('div[id="successWithWarringBootBox"]');
     successMsg = successMsg || '操作成功';
     warringMsg = warringMsg || '提示訊息';
@@ -42,7 +42,7 @@ showSuccessWithWarringBootBox = function(successMsg, warringMsg, callBack) {
 /**
  * 以 BootBox 方式顯示警示訊息
  */
-showWarringBootBox = function(warringMsg, callBack) {
+var showWarringBootBox = function(warringMsg, callBack) {
     var $warringDiv = $('div[id="warringBootBox"]');
     $('span[id="msgSpan"]', $warringDiv).empty().html(warringMsg);
 
@@ -57,7 +57,7 @@ showWarringBootBox = function(warringMsg, callBack) {
 /**
  * 以 BootBox 方式顯示失敗訊息
  */
-showErrorBootBox = function (errorMsg, callBack) {
+var showErrorBootBox = function (errorMsg, callBack) {
     $('span[id="msgSpan"]').empty().html(errorMsg);
 
     var $failDiv = $('div[id="failBootBox"]');
@@ -69,3 +69,34 @@ showErrorBootBox = function (errorMsg, callBack) {
         }
     });
 };
+
+/**
+ * 開啟等待 dialog
+ */
+var showWaitDialog = function(message) {
+    var title = '執行中...請稍後';
+
+    if(message != null && message != 'undefined' && message != '')
+    {
+        title = message;
+    }
+
+    var $waitingDiv = $('div[id="waitingDiv"]');
+
+    return bootbox.dialog({
+        title: title,
+        modal: true,
+        size: "small",
+        resizable: false,
+        closeOnEscape: false,
+        message: $waitingDiv.html(),
+        closeButton: false
+    });
+};
+
+function showWaitDialogCustom(msg) {
+    var waitDialog = showWaitDialog(msg);
+    setTimeout(function () {
+        $(waitDialog).modal('hide');
+    }, 5000);
+}
